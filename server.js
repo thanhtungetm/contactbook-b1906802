40cf6b39500ef0ed4876d263b3a25e1eb3c92006
thanhtungetm <thanhtungetm@gmail.com>
2021-09-22 13:50:53 +0700

    Cai dat xu ly loi

--- server.js
+++ server.js
@@ -2,11 +2,23 @@ const express = require('express')
 const cors  = require('cors')
 const config = require('./app/config')
 const setupContactRoutes = require('./app/routes/contact.routes')
-
+const {BadRequestError} = require('./app/helpers/errors')
 
 
 const app = express()
 setupContactRoutes(app)
+
+
+app.use((req, res, next)=>{
+    next(new BadRequestError(404, "Resource not found"))
+})
+
+app.use((err,req, res, next)=>{
+    console.log(err)
+    res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error"
+    })
+})
 
 app.use(cors({origin : config.app.origins}))
 
