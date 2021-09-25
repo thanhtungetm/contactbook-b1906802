d79351f3149c22e4c88f0d11c2e7903901552c2e
thanhtungetm <thanhtungetm@gmail.com>
2021-09-25 20:09:23 +0700

    Cai dat cac handler truy xuat CSDL

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,27 +1,134 @@
-exports.create = async (req, res)=>{
-    res.send({ message: 'create handler'})
+const { Contact} = require('../models')
+const {BadRequestError, handle} = require('../helpers/errors')
+
+exports.create = async (req, res, next)=>{
+    //Validate req
+    // console.log(req.body)
+    if(!req.body.name){
+        return next(new BadRequestError(400, "Name can not be empty."))
+    }
+
+    //Create a contact
+    const contact = new Contact({
+        name: req.body.name,
+        email: req.body.email,
+        address: req.body.address,
+        phone: req.body.phone,
+        favorite: String(req.body.favorite).toLowerCase() ==="true"
+    })
+
+    //Save contact in the database
+    const [error, document] = await handle(contact.save())
+    if (error){
+        return next(new BadRequestError(500, "An error occurred while creating the  contact"))
+    }
+
+    return res.send(document)
 }
 
-exports.findAll = async (req, res)=>{
-    res.send({ message: 'findAll handler'})
+exports.findAll = async (req, res,next)=>{
+    const condition = { }
+    const name = req.query.name;
+    if(name){
+        condition.name = {
+            $reqgex : new RegExp(name),
+            $options: 'i'
+        }
+    }
+
+    const [error, document] = await handle(Contact.find(condition))
+
+    if(error){
+        return next(new BadRequestError(500,'An error occurred while retrieving contacts'))
+    }
+    return res.send(document)
+
 }
 
-exports.findOne = async (req, res)=>{
-    res.send({ message: 'findOne handler'})
+exports.findOne = async (req, res,next)=>{
+    const condition = { 
+        _id : req.params.id
+    }
+
+    const [error, document] = await handle(Contact.find(condition))
+
+    if(error){
+        return next(new BadRequestError(500,`An retrieving contacts with id=${req.params.id}`))
+    }
+
+    if(document.length==0){
+        return next(new BadRequestError(404, 'Contact not found'))
+    }
+
+    return res.send(document)
 }
 
-exports.update = async (req, res)=>{
-    res.send({ message: 'update handler'})
+exports.update = async (req, res,next)=>{
+
+    if(Object.keys(req.body).length==0){
+        return next(new BadRequestError(400, 'Data to update can not be empty'))
+    }
+
+    const condition = {
+        _id : req.params.id,
+    }
+    const [error, document] = await handle(
+        Contact.findOneAndUpdate(condition, req.body, {
+            new : true,
+        })
+    )
+    if(error){
+        return next(new BadRequestError(500, `
+        Error updating conatct with id=${req.params.id}`))
+    }
+
+    if(!document){
+        return next(new BadRequestError(404, 'Contact not found'))
+    }
+
+    return res.send({ message : 'Contact was updated successfully'})
 }
 
-exports.delete = async (req, res)=>{
-    res.send({ message: 'delete handler'})
+exports.delete = async (req, res,next)=>{
+    const condition = {
+        _id : req.params.id,
+    }
+
+    const [error, document] = await handle(
+        Contact.findOneAndDelete(condition)
+    )
+    if(error){
+        return next(new BadRequestError(500,
+            `Could not delete contact with id=${req.params.id}`)) 
+    }
+
+    if(!document){
+        return next(new BadRequestError(404, 'Contact not found'))
+    }
+    return res.send({ message: 'Contact was deleted successfully'})
 }
 
-exports.deleteAll = async (req, res)=>{
-    res.send({ message: 'deleteAll handler'})
+exports.deleteAll = async (req, res, next)=>{
+    const [error, data] = await handle(
+        Contact.deleteMany({ })
+    )
+
+    if(error){
+        return next(new BadRequestError(500,
+            'An error occurred while removing all contacts')) 
+    }   
+
+    return res.send({ message: `${data.deletedCount} contacts  were deleted successfully`
+    })
 }
 
-exports.findAllFavorite = async (req, res)=>{
-    res.send({ message: 'findAllFavorite handler'})
+exports.findAllFavorite = async (req, res, next)=>{
+    const [error, documents] = await handle(
+        Contact.find({favorite:true})
+    )
+    if(error){
+        return next(new BadRequestError(500, 'An error occurred while retrieving favorite conntacts'))
+    }
+
+    return res.send(documents)
 }
--- server.js
+++ server.js
@@ -4,10 +4,17 @@ const config = require('./app/config')
 const setupContactRoutes = require('./app/routes/contact.routes')
 const {BadRequestError} = require('./app/helpers/errors')
 
-
 const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
 setupContactRoutes(app)
 
+app.get("/", (req, res)=>{
+    res.json({message: 'Welcome to contact book application.'})
+})
 
 app.use((req, res, next)=>{
     next(new BadRequestError(404, "Resource not found"))
@@ -22,17 +29,25 @@ app.use((err,req, res, next)=>{
 
 app.use(cors({origin : config.app.origins}))
 
-app.use(express.json())
 
-app.use(express.urlencoded({ extended:true }))
 
-app.get("/", (req, res)=>{
-    res.json({message: 'Welcome to contact book application.'})
-})
+const db = require('./app/models')
+
+db.mongoose.connect(config.db.url)
+    .then(()=>{
+        console.log("Connected to the database!")
+    })
+    .catch((err) => {
+        console.log("Cannot connect to the database!")
+        process.exit()
+    })
+
+
+
 
 const PORT = config.app.port;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, function(){
     console.log(`Server is running oon port ${PORT}`)
 
 })
